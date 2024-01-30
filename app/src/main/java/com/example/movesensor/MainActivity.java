6061dd9334fad2e6dfa785df63d6ccc7d59af658
Threx171 <alejandro_1173@hotmail.com>
2024-01-30 17:57:07 +0100

    done

---
+++ app/src/main/java/com/example/movesensor/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.movesensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private TextView txt1;
+    private TextView txt2;
+    private TextView txt3;
+    private Sensor sensor;
+    SensorEventListener sensorListener;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                // Valors de l'acceleròmetre en m/s^2
+                float xAcc = sensorEvent.values[0];
+                float yAcc = sensorEvent.values[1];
+                float zAcc = sensorEvent.values[2];
+
+                txt1 = (TextView) findViewById(R.id.textView);
+                txt2 = (TextView) findViewById(R.id.textView2);
+                txt3 = (TextView) findViewById(R.id.textView3);
+
+                // Processament o visualització de dades...
+
+                txt1.setText("X: " + xAcc);
+                txt2.setText("Y: " + yAcc);
+                txt3.setText("Z: " + zAcc);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+                // Es pot ignorar aquesta CB de moment
+            }
+        };
+
+        // Seleccionem el tipus de sensor (veure doc oficial)
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // registrem el Listener per capturar els events del sensor
+        if( sensor!=null ) {
+            sensorManager.registerListener(sensorListener,sensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+}
